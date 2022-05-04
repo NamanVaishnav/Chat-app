e49aa8d0d0049731d181b3c8f2665ec6ed04585c
Naman Vaishnav <namanmvaishnav@gmail.com>
2022-05-04 23:59:25 +0530

    returning dummy array from server

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
 import { ApolloServer, gql } from 'apollo-server'
 
-
+const users = [
+    {
+        id: 1,
+        firstname: "naman",
+        lastname: "vaishnav",
+        email: "naman@gmail.com",
+        password: "123456"
+    },
+    {
+        id: 2,
+        firstname: "jhanvi",
+        lastname: "vaishnav",
+        email: "jhanvi@gmail.com",
+        password: "123456"
+    }
+]
 
 // create schema
 const typeDefs = gql`
     type Query{
-         greet:String
+         users:[User]
+         user(id:ID!): User
     }
-`  // templet literal - able to write string in multiple line
+
+    type User{
+        id:ID
+        firstname:String
+        lastName:String
+        email:String
+   }
+` // templet literal - able to write string in multiple line
 
 
 // create resolver
-
-
-
 const resolvers = {
     Query: {
-        greet: () => "Hello World"
+        users: () => users,
+        user: (parent, { id }, context) => {
+            console.log(id);
+            return users.find(item => item.id == id);
+        }
     }
 }
 
